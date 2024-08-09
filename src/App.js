69f1bfa6c8be012ac1695b87c88e45c2f04a1c97
imarem <imar.estopa@gmail.com>
2024-08-09 10:51:18 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import "./App.css";
+import React, { useState } from "react";
+import WebView from "./WebView";
+
+// const URL = "<THEACARE_URL>/camera";
+const URL = "https://ciclopoli.theacare.de/camera";
+
+const App = () => {
+  const [image, setImage] = useState(null);
+  const handleWebViewMessage = (message) => {
+    if (!message?.imageDataUrl) {
+      console.log("Received a message without imageDataUrl");
+      return;
+    }
+    console.log(message.imageDataUrl)
+    setImage(message.imageDataUrl);
+    console.log(window)
+    if(window.ReactNativeWebView !== undefined){
+      window.ReactNativeWebView.postMessage(message.imageDataUrl);
+    }
+    
+    console.log(
+      "Received imageObj from WebView:",
+      message.width,
+      message.height,
+      message.imageDataUrl.slice(0, 20)
+    );
+  };
+
+  return (
+    <div style={{ width: "100%", height: "50vh" }}>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      {image && (
+        <img
+          src={image}
+          alt=""
+          style={{ width: "100%", height: "100%", objectFit: "contain" }}
+        />
+      )}
+      {!image && <WebView url={URL} onMessage={handleWebViewMessage} />}
+    </div>
+  );
+};
+
+export default App;
