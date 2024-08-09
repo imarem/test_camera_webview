2111b19a395b1dad510a0dac383bec7b67ac6479
imarem <imar.estopa@gmail.com>
2024-08-09 11:09:08 +0200

    FIX

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import React from "react";
 const App = () => {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
+      <button
+        onClick={() => {
+          window.ReactNativeWebView.postMessage("Hello");
+        }}
       >
-        Learn React
-      </a>
-      <button onClick={() => {window.ReactNativeWebView.postMessage('Hello', '*')}}>send to RN webview</button>
+        send to RN webview
+      </button>
+      <button
+        onClick={() => {
+          window.ReactNativeWebView.postMessage("Hello", "*");
+        }}
+      >
+        send to RN webview with "*"
+      </button>
     </div>
   );
 };
